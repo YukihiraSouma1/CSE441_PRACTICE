a2dd4f3fafb9cb5e267eb76c10c2a16f284de7e0
congc <congcut113@gmail.com>
2024-09-25 15:21:45 +0700

    add Ex01

--- Ex01/app/src/main/java/com/example/ex01/MainActivity.java
+++ Ex01/app/src/main/java/com/example/ex01/MainActivity.java
@@ -8,9 +8,7 @@ import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
