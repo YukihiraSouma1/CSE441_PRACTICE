a23252fdad0bb24d53088a0b61ecc39fd77c8120
congc <congcut113@gmail.com>
2024-10-15 16:59:23 +0700

    Ex08_2

--- Ex08_2/app/src/main/java/com/example/ex08_2/MainActivity.java
+++ Ex08_2/app/src/main/java/com/example/ex08_2/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.ex08_2;
 
+import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView imageViewPerson;
+    private ImageButton imageButtonCamera;
+    private ActivityResultLauncher<Intent> cameraLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +32,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        setupViews();
+        registerCameraLauncher();
+        imageButtonCamera.setOnClickListener(v -> {
+            Intent intent = new Intent(ACTION_IMAGE_CAPTURE);
+            if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != 0){
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, 1);
+                return;
+            }
+            cameraLauncher.launch(intent);
+        });
+    }
+
+
+    private void registerCameraLauncher() {
+        cameraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
+                imageViewPerson.setImageBitmap(bitmap);
+            }
+        });
+    }
+
+    private void setupViews() {
+        imageViewPerson = findViewById(R.id.img_view_person);
+        imageButtonCamera = findViewById(R.id.img_btn_camera);
+
     }
 }
