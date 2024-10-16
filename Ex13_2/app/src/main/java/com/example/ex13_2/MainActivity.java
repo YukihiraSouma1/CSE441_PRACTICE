285eb034776912ea9cdb7d15f5479e6037bccc31
congc <congcut113@gmail.com>
2024-10-16 20:38:44 +0700

    Ex13_2

--- Ex13_2/app/src/main/java/com/example/ex13_2/MainActivity.java
+++ Ex13_2/app/src/main/java/com/example/ex13_2/MainActivity.java
@@ -7,9 +7,6 @@ import android.widget.GridView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.List;
 
